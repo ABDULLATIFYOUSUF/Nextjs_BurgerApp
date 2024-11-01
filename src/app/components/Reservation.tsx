4519fcc1ced596a834c3895043d1ad1d2f4fc6da
ABDULLATIFYOUSUF <latifyousuf513@gmail.com>
2024-11-01 08:50:27 -0700

    second commit

--- src/app/components/Reservation.tsx
+++ src/app/components/Reservation.tsx
@@ -1,24 +1,8 @@
-"use client";
-import { useState } from "react";
+
+import React from "react";
 
 const Reservation = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [date, setDate] = useState("");
-  const [time, setTime] = useState("");
 
-  const handleReservation = () => {
-    const booker = {
-      name: name,
-      email: email,
-      date: date,
-      time: time,
-    };
-    if (booker) {
-      localStorage.setItem("booker", JSON.stringify(booker));
-      alert("Received your request, we will confirm you via Email soon");
-    }
-  };
   return (
     <section className="bg-gradient-to-r from-black to-slate-900 py-10">
       <div className="container mx-auto text-center">
@@ -32,7 +16,6 @@ const Reservation = () => {
               placeholder="Your Name..."
               className="w-full p-3 border border-gray-300 rounded-md text-black"
               required
-              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="mb-4 text-white">
@@ -41,7 +24,6 @@ const Reservation = () => {
               placeholder="Email..."
               className="w-full p-3 border border-gray-300 rounded-md text-black"
               required
-              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="mb-4 text-white">
@@ -49,7 +31,6 @@ const Reservation = () => {
               type="date"
               className="w-full p-3 border border-gray-300 rounded-md text-black"
               required
-              onChange={(e) => setDate(e.target.value)}
             />
           </div>
           <div className="mb-4 text-white">
@@ -57,11 +38,10 @@ const Reservation = () => {
               type="time"
               className="w-full p-3 border border-gray-300 rounded-md text-black"
               required
-              onChange={(e) => setTime(e.target.value)}
             />
           </div>
           <div className="w-fit mx-auto mb-4 bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700">
-            <button onClick={handleReservation}>Reserved Table</button>
+            <button>Reserved Table</button>
           </div>
         </form>
       </div>
